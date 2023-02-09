b624ffaad526db5a9c72b4da1a0c1e1fda8c2afc
Kladbm <hollykiller123@gmail.com>
2023-02-09 22:42:36 +0200

    start configuration

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
 import logging
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, ConversationHandler, \
+    CallbackQueryHandler
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+DOOM_TOWER, CLAN_BOSS, ARENA, EVENTS_TOURNAMENTS, CLAN, OTHER = range(6)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 with open('config.json') as file:
     token = json.load(file)
@@ -36,113 +36,131 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("It's /help command")
 
 
-async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    update_message_text = update.message.text
-
-    default_parameter = [
-        InlineKeyboardButton("Ок", callback_data="1"),
-        InlineKeyboardButton("Заново", callback_data="1"),
-        InlineKeyboardButton("Закрыть", callback_data="1")
-    ]
-
-    if update_message_text == "Роковая башня":
+async def message_doom_tower(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "Роковая башня":
         keyboard = [
             [
-                InlineKeyboardButton("Легкая", callback_data="1"),
-                InlineKeyboardButton("Сложная", callback_data="1")
+                InlineKeyboardButton("Легкая", callback_data=1),
+                InlineKeyboardButton("Сложная", callback_data=1)
             ],
-            default_parameter
+            [
+                InlineKeyboardButton("Закрыть", callback_data=1)
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("Роковая башня", reply_markup=reply_markup)
-        return
+        return DOOM_TOWER
 
-    if update_message_text == "Клан босс":
+
+async def message_clan_boss(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "Клан босс":
         keyboard = [
             [
-                InlineKeyboardButton("4 КБ", callback_data="1"),
-                InlineKeyboardButton("5 КБ", callback_data="1"),
-                InlineKeyboardButton("6 КБ", callback_data="1")
+                InlineKeyboardButton("4 КБ", callback_data=1),
+                InlineKeyboardButton("5 КБ", callback_data=1),
+                InlineKeyboardButton("6 КБ", callback_data=1)
             ],
-            default_parameter
+            [
+                InlineKeyboardButton("Закрыть", callback_data=1)
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("Клан босс", reply_markup=reply_markup)
-        return
+        return CLAN_BOSS
 
-    if update_message_text == "Арена":
+
+async def message_arena(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "Арена":
         keyboard = [
             [
-                InlineKeyboardButton("Обычная", callback_data="1"),
-                InlineKeyboardButton("Групповая", callback_data="1")
+                InlineKeyboardButton("Обычная", callback_data=1),
+                InlineKeyboardButton("Групповая", callback_data=1)
             ],
-            default_parameter
+            [
+                InlineKeyboardButton("Закрыть", callback_data=1)
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("Арена", reply_markup=reply_markup)
-        return
+        return ARENA
 
-    if update_message_text == "События / Турниры":
+
+async def message_events_tournaments(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "События / Турниры":
         keyboard = [
             [
-                InlineKeyboardButton("События", callback_data="1"),
-                InlineKeyboardButton("Турниры", callback_data="1")
+                InlineKeyboardButton("События", callback_data=1),
+                InlineKeyboardButton("Турниры", callback_data=1)
             ],
-            default_parameter
+            [
+                InlineKeyboardButton("Закрыть", callback_data=1)
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("События / Турниры", reply_markup=reply_markup)
-        return
+        return EVENTS_TOURNAMENTS
 
-    if update_message_text == "Клан":
+
+async def message_clan(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "Клан":
         keyboard = [
             [
-                InlineKeyboardButton("Турнир кланов", callback_data="1")
+                InlineKeyboardButton("Турнир кланов", callback_data=1)
             ],
             [
-                InlineKeyboardButton("Сундук", callback_data="1"),
-                InlineKeyboardButton("Магазин", callback_data="1")
+                InlineKeyboardButton("Сундук", callback_data=1),
+                InlineKeyboardButton("Магазин", callback_data=1)
             ],
-            default_parameter
+            [
+                InlineKeyboardButton("Закрыть", callback_data=1)
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("Клан", reply_markup=reply_markup)
-        return
+        return CLAN
 
-    if update_message_text == "Другое":
+
+async def message_other(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    if update.message.text == "Другое":
         keyboard = [
             [
-                InlineKeyboardButton("Подземка", callback_data="1"),
-                InlineKeyboardButton("Рынок", callback_data="1")
+                InlineKeyboardButton("Подземка", callback_data=1),
+                InlineKeyboardButton("Рынок", callback_data=1)
             ],
             [
-                InlineKeyboardButton("Вход", callback_data="1"),
-                InlineKeyboardButton("Миссии", callback_data="1")
+                InlineKeyboardButton("Вход", callback_data=1),
+                InlineKeyboardButton("Миссии", callback_data=1)
             ],
             [
-                InlineKeyboardButton("Задания", callback_data="1"),
-                InlineKeyboardButton("Магазин", callback_data="1")
+                InlineKeyboardButton("Задания", callback_data=1),
+                InlineKeyboardButton("Магазин", callback_data=1)
             ],
-            default_parameter
+            [
+                InlineKeyboardButton("Закрыть", callback_data=1)
+            ]
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("Другое", reply_markup=reply_markup)
-        return
+        return OTHER
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная команда")
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text="До встречи")
+    return ConversationHandler.END
 
 
 if __name__ == '__main__':
@@ -154,7 +172,40 @@ if __name__ == '__main__':
     help_handler = CommandHandler('help', help)
     application.add_handler(help_handler)
 
-    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
-    application.add_handler(message_handler)
+    # message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
+    # application.add_handler(message_handler)
+
+    conversation_handler = ConversationHandler(
+        entry_points=[
+            MessageHandler(filters.TEXT & (~filters.COMMAND), message_clan_boss),
+            MessageHandler(filters.TEXT & (~filters.COMMAND), message_doom_tower),
+            MessageHandler(filters.TEXT & (~filters.COMMAND), message_arena),
+            MessageHandler(filters.TEXT & (~filters.COMMAND), message_events_tournaments),
+            MessageHandler(filters.TEXT & (~filters.COMMAND), message_clan),
+            MessageHandler(filters.TEXT & (~filters.COMMAND), message_other)
+        ],
+        states={
+            DOOM_TOWER: [
+                CallbackQueryHandler(end, pattern="^" + "1" + "$")
+            ],
+            CLAN_BOSS: [
+                CallbackQueryHandler(end, pattern="^" + "1" + "$")
+            ],
+            ARENA: [
+                CallbackQueryHandler(end, pattern="^" + "1" + "$")
+            ],
+            EVENTS_TOURNAMENTS: [
+                CallbackQueryHandler(end, pattern="^" + "1" + "$")
+            ],
+            CLAN: [
+                CallbackQueryHandler(end, pattern="^" + "1" + "$")
+            ],
+            OTHER: [
+                CallbackQueryHandler(end, pattern="^" + "1" + "$")
+            ]
+        },
+        fallbacks=[CommandHandler("start", start)]
+    )
+    application.add_handler(conversation_handler)
 
     application.run_polling()
