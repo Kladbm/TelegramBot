d1e5a81524afa0d9345e562970b531c1fb941b9a
Kladbm <hollykiller123@gmail.com>
2023-02-09 17:43:35 +0200

    start configuration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import logging
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, InlineQueryHandler
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,16 +13,136 @@ with open('config.json') as file:
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="It's /start command")
+    buttons = [
+        [
+            KeyboardButton("Роковая башня"),
+            KeyboardButton("Клан босс")
+        ],
+        [
+            KeyboardButton("Арена"),
+            KeyboardButton("События / Турниры")
+        ],
+        [
+            KeyboardButton("Клан"),
+            KeyboardButton("Другое")
+        ]
+    ]
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="It's /start command",
+                                   reply_markup=ReplyKeyboardMarkup(buttons, resize_keyboard=True))
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="It's /help command")
+    await update.message.reply_text("It's /help command")
 
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    update_message_text = update.message.text
+
+    default_parameter = [
+        InlineKeyboardButton("Ок", callback_data="1"),
+        InlineKeyboardButton("Заново", callback_data="1"),
+        InlineKeyboardButton("Закрыть", callback_data="1")
+    ]
+
+    if update_message_text == "Роковая башня":
+        keyboard = [
+            [
+                InlineKeyboardButton("Легкая", callback_data="1"),
+                InlineKeyboardButton("Сложная", callback_data="1")
+            ],
+            default_parameter
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Роковая башня", reply_markup=reply_markup)
+        return
+
+    if update_message_text == "Клан босс":
+        keyboard = [
+            [
+                InlineKeyboardButton("4 КБ", callback_data="1"),
+                InlineKeyboardButton("5 КБ", callback_data="1"),
+                InlineKeyboardButton("6 КБ", callback_data="1")
+            ],
+            default_parameter
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Клан босс", reply_markup=reply_markup)
+        return
+
+    if update_message_text == "Арена":
+        keyboard = [
+            [
+                InlineKeyboardButton("Обычная", callback_data="1"),
+                InlineKeyboardButton("Групповая", callback_data="1")
+            ],
+            default_parameter
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Арена", reply_markup=reply_markup)
+        return
+
+    if update_message_text == "События / Турниры":
+        keyboard = [
+            [
+                InlineKeyboardButton("События", callback_data="1"),
+                InlineKeyboardButton("Турниры", callback_data="1")
+            ],
+            default_parameter
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("События / Турниры", reply_markup=reply_markup)
+        return
+
+    if update_message_text == "Клан":
+        keyboard = [
+            [
+                InlineKeyboardButton("Турнир кланов", callback_data="1")
+            ],
+            [
+                InlineKeyboardButton("Сундук", callback_data="1"),
+                InlineKeyboardButton("Магазин", callback_data="1")
+            ],
+            default_parameter
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Клан", reply_markup=reply_markup)
+        return
+
+    if update_message_text == "Другое":
+        keyboard = [
+            [
+                InlineKeyboardButton("Подземка", callback_data="1"),
+                InlineKeyboardButton("Рынок", callback_data="1")
+            ],
+            [
+                InlineKeyboardButton("Вход", callback_data="1"),
+                InlineKeyboardButton("Миссии", callback_data="1")
+            ],
+            [
+                InlineKeyboardButton("Задания", callback_data="1"),
+                InlineKeyboardButton("Магазин", callback_data="1")
+            ],
+            default_parameter
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Другое", reply_markup=reply_markup)
+        return
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная команда")
 
 
 if __name__ == '__main__':
@@ -34,7 +154,7 @@ if __name__ == '__main__':
     help_handler = CommandHandler('help', help)
     application.add_handler(help_handler)
 
-    caps_handler = CommandHandler('caps', caps)
-    application.add_handler(caps_handler)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
+    application.add_handler(message_handler)
 
     application.run_polling()
